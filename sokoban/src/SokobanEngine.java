04f34be13382fb1b81dcb1cc2fd6ac019bbf6a0d
beatanowakowska <the.beata.nowakowska@gmail.com>
2016-03-03 11:22:52 +0100

    Ruch w prawo ze sprawdzaniem czy sciana blokuje.

--- sokoban/src/SokobanEngine.java
+++ sokoban/src/SokobanEngine.java
@@ -1,14 +1,79 @@
-﻿
+﻿import java.util.Arrays;
+
 public class SokobanEngine {
 
+	private static final char GAMER = '@';
+	private static final char EMPTY = ' ';
+	private static final char BOX = 'o';
+	private static final char WALL = '#';
+	
 	private char[][] board;
 
+	private int xGamerPossition = -1;
+	private int yGamerPossition = -1;
+	
+	
 	public void start(char[][] board) {
-		this.board = board;
+		this.board = copyBoard(board);
+		determineGamerPossition();
 	}
 
 	public char[][] getBoard() {
-		return board;
+		return copyBoard(board);
+	}
+
+	private char [][] copyBoard(char [][] board){
+		char [][] copy = new char[board.length][];
+		
+		for (int y = 0; y < board.length; y++){
+			copy[y] = Arrays.copyOf(board[y], board[y].length);
+		}
+		
+		return copy;
+	}
+	
+	public void moveGamerRight() {
+		
+		if (!isMoveValid()){ 
+			return;
+		}
+		
+		board[yGamerPossition][xGamerPossition] = EMPTY;
+		board[yGamerPossition][xGamerPossition + 1] = GAMER;
+		board[yGamerPossition][xGamerPossition + 2] = BOX;
+		
+		xGamerPossition++;
+	}
+
+	private boolean isMoveValid() {
+		if (isBoxBlockedByWall())
+			return false;
+		
+		if (isGamerBlockedByWall()) {
+			return false;
+		}
+		return true;
+	}
+
+	private boolean isGamerBlockedByWall() {
+		return board[yGamerPossition][xGamerPossition + 1] == WALL;
+	}
+
+	private boolean isBoxBlockedByWall() {
+		return board[yGamerPossition][xGamerPossition + 1] == BOX &&
+				board[yGamerPossition][xGamerPossition + 2] == WALL;
+	}
+
+	private void determineGamerPossition() {
+		for (int y = 0; y < board.length; y++){
+			for (int x = 0; x < board[y].length; x++){
+				if (board[y][x] == GAMER){
+					xGamerPossition = x;
+					yGamerPossition = y;
+					return;
+				}
+			}
+		}
 	}
 
 }
--- sokoban/test/SokobanEngineTest.java
+++ sokoban/test/SokobanEngineTest.java
@@ -9,8 +9,18 @@ public class SokobanEngineTest {
 		"#@o.#".toCharArray(),
 		"#####".toCharArray() };
 
+	private char[][] simplestBoardWon = new char[][] { 
+		"#####".toCharArray(), 
+		"# @o#".toCharArray(),
+		"#####".toCharArray() };
+
+	private char[][] boardWallInFront = new char[][] { 
+		"#####".toCharArray(), 
+		"#.o@#".toCharArray(),
+		"#####".toCharArray() };
+
 	@Before
-	public void TestInitialize() {
+	public void testInitialize() {
 		sokobanEngine = new SokobanEngine();
 	}
 
@@ -21,13 +31,63 @@ public class SokobanEngineTest {
 		// when
 		char[][] result = sokobanEngine.getBoard();
 		// then
-		assertBoard(simplestBoard, result);
+		assertBoardEquals(simplestBoard, result);
 	}
 
-	private void assertBoard(char[][] simplestBoard, char[][] result) {
+	private void assertBoardEquals(char[][] simplestBoard, char[][] result) {
 		for (int y = 0; y < simplestBoard.length; y++) {
 			Assert.assertArrayEquals(simplestBoard[y], result[y]);
 		}
 	}
-
+	
+	@Test
+	public void gamerCanMoveBoxRight(){
+		//given
+		sokobanEngine.start(simplestBoard);
+		// when
+		sokobanEngine.moveGamerRight();
+		char[][] result = sokobanEngine.getBoard();
+		// then
+		assertBoardEquals(simplestBoardWon, result);
+	}
+	
+	@Test
+	public void gameDoesNotUseInitialBoard() {
+		//given
+		sokobanEngine.start(simplestBoard);
+		// when
+		char[][] result = sokobanEngine.getBoard();
+		//then
+		Assert.assertNotSame(simplestBoard, result);
+	}
+	
+	@Test
+	public void gameDoesNotRevealItsState() {
+		//given
+		sokobanEngine.start(simplestBoard);
+		// when then
+		Assert.assertNotSame(sokobanEngine.getBoard(), sokobanEngine.getBoard());
+	}
+	
+	@Test
+	public void gamerCantMoveBoxOnTheWall(){
+		//given
+		sokobanEngine.start(simplestBoardWon);
+		// when
+		sokobanEngine.moveGamerRight();
+		char[][] result = sokobanEngine.getBoard();
+		// then
+		assertBoardEquals(simplestBoardWon, result);
+	}
+	
+	@Test
+	public void gamerCantMoveTheWallBeforeHim(){
+		//given
+		sokobanEngine.start(boardWallInFront);
+		// when
+		sokobanEngine.moveGamerRight();
+		char[][] result = sokobanEngine.getBoard();
+		// then
+		assertBoardEquals(boardWallInFront, result);
+	}
 }
